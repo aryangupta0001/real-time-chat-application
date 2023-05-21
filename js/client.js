5563c9b41a0cac6d7b1410262b9f0f40cf0a12c6
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-05-21 23:51:55 +0530

    fixed CORS error problem.

--- js/client.js
+++ js/client.js
@@ -2,8 +2,19 @@ const socket = io("http://localhost:8000");
 
 const form = document.getElementById("send_message");
 const message = document.getElementById("msg");
-const messagecontainer = document.querySelector("container");
+const messagecontainer = document.querySelector(".container");
 
 const user_name = prompt("What's your name ?");
-console.log(user_name);
 socket.emit("new-user-joined", user_name);
+
+
+const append_user = (message, position) => {
+    const message_element = document.createElement("div");
+    message_element.innerText = message;
+    message_element.classList.add("message", position);
+    messagecontainer.append(message_element);
+}
+
+socket.on("user-joined", user_name=> {
+    append_user(`${user_name} joined the chat`, "center");
+})
